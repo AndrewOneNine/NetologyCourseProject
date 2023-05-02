e10d5bfbe3f5fc9e3f93eff81cc375539aa97875
Andrey S <archer.archi@bk.ru>
2023-05-02 11:36:04 +0300

    Curse ready

--- personal.py
+++ personal.py
@@ -5,10 +5,10 @@ version='5.131'
 yd_photo_count=5
 
 # Токен для VK
-vk_token='vk1.a.zk9gbNM9U1dxn085Ptnqvbx4BTViyJZlchlho3xSYvmC8Pofux3NvXlATTm6qzAQ_tTAUHzKDTJbgXG_6z32zL-GemetYZWyrn6q7K--4seSLjtYZMKZhsWby1NDY4023DlCvnH0eLUyl4zskUNEa1pUAM5c_pThf82O8InjWdEdoX0Wcmo-FAYfXIcgTm7ml8DoCYP4Gq-RgyH6Qu1Nyg'
+vk_token=''
 
 # ID пользователя, у которого берем фото
 owner_id='79368374'
 
 # Токен для Яндекс-Диска
-yd_token='y0_AgAAAAAkfasPAADLWwAAAADfFnwbEUa7SDoKRVqZBhgxj9F-areoN6g'
+yd_token=''
